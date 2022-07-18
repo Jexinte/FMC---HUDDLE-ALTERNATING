b556c4881bcffd756dba1877e78cfd9dc3ba4e7a
Yokke <mdembelepro@gmail.com>
2022-07-18 11:28:04 +0200

    config for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/huddle-landing-page-with-alternating-feature-blocks-master'
 })
